fd3b925223c2947932578622e492f5296a8f98b9
lukas <lukas@DESKTOP-2TCT2D0>
2018-06-04 22:40:14 +0200

    Initial commit

---
+++ src/main/java/com/gpd2020/small_enterprise_app/SmallEnterpriseAppApplication.java
@@ -0,0 +1,12 @@
+package com.gpd2020.small_enterprise_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallEnterpriseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallEnterpriseAppApplication.class, args);
+	}
+}
